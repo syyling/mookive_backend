a50fcf752cfd2d141e90d2476f48d6da9e25e0fb
seo-young <sy0hiu@gmail.com>
2024-02-15 15:34:07 +0900

    Initial commit

---
+++ src/main/java/com/mookive/mookive_backend/MookiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mookive.mookive_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MookiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MookiveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mookive/mookive_backend/MookiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mookive.mookive_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MookiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
