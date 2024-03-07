8a7f290d5dd6d919c02614750b5568f0bedaf0e2
seo-young <sy0hiu@gmail.com>
2024-03-07 14:18:38 +0900

    [feat] Playlist 도메인, repository 추가

---
+++ src/main/java/com/mookive/mookive_backend/playlist/domain/repository/PlaylistRepository.java
@@ -0,0 +1,7 @@
+package com.mookive.mookive_backend.playlist.domain.repository;
+
+import com.mookive.mookive_backend.playlist.domain.entity.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+}
