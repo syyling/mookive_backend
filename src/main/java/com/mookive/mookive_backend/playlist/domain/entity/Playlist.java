326b7ce97b2916aeac34d30dd95197bf9f2e6233
syyling <sy0hiu@gmail.com>
2024-03-26 02:17:13 +0900

    [feat] 플레이리스트 이름 수정 기능 추가

--- src/main/java/com/mookive/mookive_backend/playlist/domain/entity/Playlist.java
+++ src/main/java/com/mookive/mookive_backend/playlist/domain/entity/Playlist.java
@@ -1,11 +1,15 @@
 package com.mookive.mookive_backend.playlist.domain.entity;
 
+import com.mookive.mookive_backend.movieInPlaylist.domain.entity.MovieInPlaylist;
 import com.mookive.mookive_backend.user.domain.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,9 +25,16 @@ public class Playlist {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @OneToMany(mappedBy = "playlist", cascade = CascadeType.REMOVE)
+    private List<MovieInPlaylist> movieList = new ArrayList<>();
+
     @Builder
     public Playlist(String name, User user) {
         this.name = name;
         this.user = user;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
     }
 }
